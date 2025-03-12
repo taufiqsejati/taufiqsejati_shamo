2ffb448c6a88b59e6a2a2566fe25633447c794f5
taufiqsejati <taufiq_sejati@rocketmail.com>
2025-03-12 14:33:27 +0700

    update

--- lib/pages/home/main_page.dart
+++ lib/pages/home/main_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+// import 'package:provider/provider.dart';
 import 'package:shamo/pages/home/chat_page.dart';
 import 'package:shamo/pages/home/home_page.dart';
 import 'package:shamo/pages/home/profile_page.dart';
@@ -34,73 +34,86 @@ class _MainPageState extends State<MainPage> {
       return ClipRRect(
         borderRadius: BorderRadius.vertical(top: Radius.circular(30)),
         child: BottomAppBar(
+          color: backgroundColor4,
           shape: CircularNotchedRectangle(),
+          // height: 80,
           notchMargin: 12,
+          padding: EdgeInsets.zero,
           clipBehavior: Clip.antiAlias,
-          child: BottomNavigationBar(
-            backgroundColor: backgroundColor4,
-            currentIndex: currentIndex,
-            // currentIndex: pageProvider.currentIndex,
-            onTap: (value) {
-              print(value);
-              currentIndex = value;
-              // pageProvider.currentIndex = value;
-            },
-            type: BottomNavigationBarType.fixed,
-            items: [
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: EdgeInsets.only(top: 20, bottom: 10),
-                  child: Image.asset(
-                    'assets/icon_home.png',
-                    width: 21,
-                    color:
-                        // pageProvider.currentIndex == 0
-                        currentIndex == 0 ? primaryColor : Color(0xff808191),
+          child: Theme(
+            data: Theme.of(context).copyWith(
+              splashColor: Colors.transparent,
+              highlightColor: Colors.transparent,
+              focusColor: Colors.transparent,
+              shadowColor: Colors.transparent,
+            ),
+            child: BottomNavigationBar(
+              backgroundColor: backgroundColor4,
+              elevation: 0,
+              currentIndex: currentIndex,
+              // currentIndex: pageProvider.currentIndex,
+              onTap: (value) {
+                print(value);
+                currentIndex = value;
+                setState(() {});
+                // pageProvider.currentIndex = value;
+              },
+              type: BottomNavigationBarType.fixed,
+              items: [
+                BottomNavigationBarItem(
+                  icon: Container(
+                    margin: EdgeInsets.only(top: 10, bottom: 0),
+                    child: Image.asset(
+                      'assets/icon_home.png',
+                      width: 21,
+                      color:
+                          // pageProvider.currentIndex == 0
+                          currentIndex == 0 ? primaryColor : Color(0xff808191),
+                    ),
                   ),
+                  label: '',
                 ),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: EdgeInsets.only(top: 20, bottom: 10),
-                  child: Image.asset(
-                    'assets/icon_chat.png',
-                    width: 20,
-                    color:
-                        // pageProvider.currentIndex == 1
-                        currentIndex == 1 ? primaryColor : Color(0xff808191),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    margin: EdgeInsets.only(top: 10, bottom: 0),
+                    child: Image.asset(
+                      'assets/icon_chat.png',
+                      width: 20,
+                      color:
+                          // pageProvider.currentIndex == 1
+                          currentIndex == 1 ? primaryColor : Color(0xff808191),
+                    ),
                   ),
+                  label: '',
                 ),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: EdgeInsets.only(top: 20, bottom: 10),
-                  child: Image.asset(
-                    'assets/icon_wishlist.png',
-                    width: 20,
-                    color:
-                        // pageProvider.currentIndex == 2
-                        currentIndex == 2 ? primaryColor : Color(0xff808191),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    margin: EdgeInsets.only(top: 10, bottom: 0),
+                    child: Image.asset(
+                      'assets/icon_wishlist.png',
+                      width: 20,
+                      color:
+                          // pageProvider.currentIndex == 2
+                          currentIndex == 2 ? primaryColor : Color(0xff808191),
+                    ),
                   ),
+                  label: '',
                 ),
-                label: '',
-              ),
-              BottomNavigationBarItem(
-                icon: Container(
-                  margin: EdgeInsets.only(top: 20, bottom: 10),
-                  child: Image.asset(
-                    'assets/icon_profile.png',
-                    width: 18,
-                    color:
-                        // pageProvider.currentIndex == 3
-                        currentIndex == 3 ? primaryColor : Color(0xff808191),
+                BottomNavigationBarItem(
+                  icon: Container(
+                    margin: EdgeInsets.only(top: 10, bottom: 0),
+                    child: Image.asset(
+                      'assets/icon_profile.png',
+                      width: 18,
+                      color:
+                          // pageProvider.currentIndex == 3
+                          currentIndex == 3 ? primaryColor : Color(0xff808191),
+                    ),
                   ),
+                  label: '',
                 ),
-                label: '',
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       );
